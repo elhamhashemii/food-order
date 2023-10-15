85e53abeecd9dfb2cd1ffbce79bb8a76c93e39e4
elhamhashemii <hashemi.elham22@gmail.com>
2023-10-15 12:12:21 +0330

    btn animation added using useEffect hook

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,29 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 const HeaderCartButton = (props) => {
+  const [btnIsBumped, setBtnIsBumped] = useState(false);
   const ctx = useContext(CartContext);
+  const { items } = ctx;
   const numberOfCartrItems = ctx.items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
+  const btnClasses = `${classes.button} ${btnIsBumped && classes.bump}`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsBumped(true);
+    const timer = setTimeout(() => {
+      setBtnIsBumped(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button onClick={props.onClick} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
